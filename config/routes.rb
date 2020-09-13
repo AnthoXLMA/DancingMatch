7f59d321ddd678a7abe549099ca59f2ae741778f
AnthoXLMA <contact@service-lite-plus.com>
2020-09-13 12:55:28 +0200

    routes1

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,14 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # devise_for :users
   # devise_for :partners
-
-
   resource :profile, only: [:edit, :update, :show]
-
-  resources :dances, only: [:index, :show] do
-    resources :appointments, only: [:create]
 
   resources :dances, only: [:index, :show, :new, :create] do
     resource :dancers, only: [:edit, :update, :show]
-
   end
+
+  resources :partners, only: [:index, :show] do
+  resources :appointments, only: [:create]
 
   namespace :partners do
     resources :dances, only: [:index, :show, :new, :create]
@@ -24,7 +21,8 @@ Rails.application.routes.draw do
         patch :refuse
 
   resources :partners, only: [:destroy]
-end
-end
+      end
+    end
+  end
 end
 end
