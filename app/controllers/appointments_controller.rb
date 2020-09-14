5b55c533999f1f5d217b88c11563a164a3c91f6a
AnthoXLMA <contact@service-lite-plus.com>
2020-09-14 10:56:48 +0200

    models

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,4 +1,9 @@
 class AppointmentsController < ApplicationController
   def create
+    @appointment = Appointment.new
+    @appointment.user = current_user
+    @partner = Partner.find(params[:partner_id])
+    @appointment.partner = @partner
+    redirect_to partner_path(@partner, applied: true) if @appointment.save
   end
 end
