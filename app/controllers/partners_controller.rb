a3ca3be21062d5fe301ea16f95fac298514e08a1
AnthoXLMA <contact@service-lite-plus.com>
2020-09-16 17:01:10 +0200

    newessai

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -17,6 +17,8 @@ class PartnersController < ApplicationController
 
   def show
     @partner = Partner.find(params[:id])
-    @style = Dance.find(params[:id])
+    # @dance = Dance.new
+    # @dancer  = Dance.find(dance: params[:id])
+    # @style = @partner.dances
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_111109) do
+ActiveRecord::Schema.define(version: 2020_09_16_133135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 2020_09_16_111109) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "partner_id"
+    t.index ["partner_id"], name: "index_dances_on_partner_id"
   end
 
   create_table "partners", force: :cascade do |t|
@@ -64,8 +66,7 @@ ActiveRecord::Schema.define(version: 2020_09_16_111109) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
     t.string "password"
-    t.bigint "dance_id"
-    t.index ["dance_id"], name: "index_partners_on_dance_id"
+    t.string "dance"
   end
 
   create_table "users", force: :cascade do |t|
@@ -84,5 +85,5 @@ ActiveRecord::Schema.define(version: 2020_09_16_111109) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "appointments", "partners"
   add_foreign_key "appointments", "users"
-  add_foreign_key "partners", "dances"
+  add_foreign_key "dances", "partners"
 end
