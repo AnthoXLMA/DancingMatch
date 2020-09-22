8d7e5b5189ac973f67178757ef727f68051490f3
AnthoXLMA <contact@service-lite-plus.com>
2020-09-22 15:51:23 +0200

    user dances + CSS

--- db/migrate/20200913101747_create_dances.rb
+++ db/migrate/20200913101747_create_dances.rb
@@ -2,7 +2,7 @@ class CreateDances < ActiveRecord::Migration[6.0]
   def change
     create_table :dances do |t|
       t.string :title
-      # t.references :users
+      t.references :user
       t.timestamps
     end
   end
