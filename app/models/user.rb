f1598786d566c64dbf7d2fc59f04e34f60f573dd
AnthoXLMA <contact@service-lite-plus.com>
2021-03-22 12:31:12 +0100

    changes OK

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one_attached :profile_pic
-  # belongs_to :dance
+  # has_one_attached :profile_pic
   has_many :dances
-  has_many :messages, through: :chatrooms
-  has_many :appointments
-  has_many :partners, through: :appointments
-  geocoded_by :location
-  after_validation :geocode, if: :will_save_change_to_location?
+  # belongs_to :dance
+  # has_many :messages, through: :chatrooms
+  # has_many :appointments
+  # has_many :partners, through: :appointments
+  # geocoded_by :location
+  # after_validation :geocode, if: :will_save_change_to_location?
   # validates :pseudo, presence: true
 end
--- db/migrate/20201003150330_add_fields_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddFieldsToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :terms, :boolean
-    add_column :users, :location, :string
-  end
-end
