0ed0e1ae63457b5a833bb34620ca9546c1f31c0f
AnthoXLMA <contact@service-lite-plus.com>
2021-07-17 05:13:28 +0200

    profile pgoto title

--- app/javascript/plugins/select.js
+++ app/javascript/plugins/select.js
@@ -1,19 +1,19 @@
 // TODO: React to a click on the button!
-// const button = document.querySelector('#clickme');
-// const audio = new Audio('sound.mp3');
+const button = document.querySelector('#clickme');
+const audio = new Audio('sound.mp3');
 
-// button.addEventListener('click', (e) => {
-//   e.target.classList.add('disabled');
-//   e.target.innerText = 'Bingo!';
-//   audio.play();
-// });
+button.addEventListener('click', (e) => {
+  e.target.classList.add('disabled');
+  e.target.innerText = 'Bingo!';
+  audio.play();
+});
 
 
 
-// const salsaElement = () => {
-//   // TODO 2: select the France list item and return it!
-//   const salsa = document.getElementById('salsa');
-//   return salsa;
-// };
+const salsaElement = () => {
+  // TODO 2: select the France list item and return it!
+  const salsa = document.getElementById('salsa');
+  return salsa;
+};
 
-// module.exports = salsaElement; // Do not remove.
+module.exports = salsaElement; // Do not remove.
