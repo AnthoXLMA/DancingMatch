2ad99e9ceeaac34417f2229da767eea28a831ed4
AnthoXLMA <contact@service-lite-plus.com>
2021-07-17 15:18:18 +0200

    photo active storage in event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_12_232852) do
+ActiveRecord::Schema.define(version: 2021_07_17_130544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2021_07_12_232852) do
     t.string "name"
     t.float "latitude"
     t.float "longitude"
+    t.string "pic"
     t.index ["dance_id"], name: "index_appointments_on_dance_id"
     t.index ["user_id"], name: "index_appointments_on_user_id"
   end
