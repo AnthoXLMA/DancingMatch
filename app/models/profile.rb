17e11e9407ca4c75650221fd703f71b712caceeb
AnthoXLMA <contact@service-lite-plus.com>
2021-09-15 19:35:34 +0200

    profile ID war

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,6 +5,4 @@ class Profile < ApplicationRecord
   has_many :dance_id
   has_many :dances
   has_many :appointments
-  geocoded_by :location
-  after_validation :geocode, if: :will_save_change_to_location?
 end
