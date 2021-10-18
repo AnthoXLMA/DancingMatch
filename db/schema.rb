e9de0b4457f9503234c53d686a1385ed5fef53c6
AnthoXLMA <contact@service-lite-plus.com>
2021-10-18 17:53:30 +0200

    profile unique/ a voir doublon des profils

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_14_183738) do
+ActiveRecord::Schema.define(version: 2021_10_18_105527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 2021_10_14_183738) do
     t.bigint "dance_id"
     t.string "investissement"
     t.string "niveau"
+    t.integer "training_per_week"
     t.index ["dance_id"], name: "index_profiles_on_dance_id"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
