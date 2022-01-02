2084cfb5ac8d7b47d8f7390cb281ff6cda4e5c10
AnthoXLMA <contact@service-lite-plus.com>
2022-01-02 12:00:42 +0100

    css_image

--- app/javascript/plugins/init_chart.js
+++ app/javascript/plugins/init_chart.js
@@ -1,32 +1,31 @@
 const initChart = () => {
-  const chartElement    = document.getElementById('graph1');
+  const chartElement    = document.getElementById('graph1').getContext('2d');
   if (chartElement) {
-    const userName      = chartElement.dataset.userFullname;
-    const profileTitle    = chartElement.dataset.offerTitle;
-    const userSkills    = JSON.parse(chartElement.dataset.userSkills);
+    const userName        = chartElement.dataset.userFullname;
+    const profileTitle    = chartElement.dataset.profileTitle;
+    const profileStars    = JSON.parse(chartElement.dataset.profileStars);
     const profileSkills   = JSON.parse(chartElement.dataset.profileSkills);
-    const skillNames    = JSON.parse(chartElement.dataset.skillNames);
-    const ctx           = chartElement.getContext('2d');
+    const skillNames      = JSON.parse(chartElement.dataset.skillNames);
+    // const ctx             = chartElement.getContext('2d');
     const data = {
         // The data for our dataset
             labels: skillNames,
             datasets: [
             {
                 label: userName,
-                //backgroundColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgb(255, 99, 132)',
                 borderColor: '#2f97c1',
-                //borderWidth:
+                borderWidth:
                 data: userSkills
             },
             {
                 label: profileTitle,
-                //backgroundColor: 'rgb(25, 99, 132)',
+                backgroundColor: 'rgb(25, 99, 132)',
                 borderColor: '#222222',
                 data: profileSkills
             }
           ]
         };
-
         // Configuration options go here
     const options = {
       responsive: true,
