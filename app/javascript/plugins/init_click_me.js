281530e0bf2ad35677f6dcaf3518a53f713adfc7
AnthoXLMA <contact@service-lite-plus.com>
2022-01-25 23:25:23 +0100

    javascript 1

--- app/javascript/plugins/init_click_me.js
+++ app/javascript/plugins/init_click_me.js
@@ -1,10 +1,9 @@
 const initClickMe = () => {
-  const subscription = document.querySelector("#click-participate");
-    subscription.addEventListener('click', (event) => {
-  event = document.querySelector(".information-event h3");
-  event.insertAdjacentHTML('beforeend', (event));
-  console.log(event)
-  });
-};
-
-export { initClickMe } ;
+  const subscription = document.getElementById("#click-participate");
+  //   subscription.addEventListener('click', (event) => {
+  //   event = document.querySelector("#event-uptitle h3");
+  //     event.insertAdjacentHTML('beforeend', 'hello');
+  // });
+    console.log('hello')
+}
+export { initClickMe };
