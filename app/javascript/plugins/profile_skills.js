5eec059043fe39875597ed87bd24988bc15d7989
AnthoXLMA <contact@service-lite-plus.com>
2022-02-08 14:00:30 +0100

    allgraaphs

--- app/javascript/plugins/profile_skills.js
+++ app/javascript/plugins/profile_skills.js
@@ -1,9 +1,13 @@
 function initSkills () {
+  //SELECTIONNER TOUS LES ELEMENTS TABLEAUX
     let allGraphs = document.querySelectorAll('#graph1');
-    const graph = allGraphs.forEach(x => {
-      const mySkills   = JSON.parse(x.dataset.userSkills);
-      const skillsnaming    = JSON.parse(x.dataset.skillsName);
-      const graph = new Chart(x, {
+    // SEPARER CHAQUE ELEMENT DE l'ARRAY ALLGRAPHS
+    const graph = allGraphs.forEach(array => {
+      // PARSER LES DONNEES COMPETENCES ENONCEES DANS LA VUE
+      const mySkills   = JSON.parse(array.dataset.userSkills);
+      //
+      const skillsnaming    = JSON.parse(array.dataset.skillsName);
+      const graph = new Chart(array, {
       type: 'line',
       data: {
       labels: skillsnaming,
@@ -27,8 +31,8 @@ function initSkills () {
           }
         }
       }
-    });
-  }
+    }
+  )}
 )};
 
 export { initSkills };
