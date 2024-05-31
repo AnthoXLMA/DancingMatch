c90b3ebf611853d25a8577c191e140d3bd938136
Anthony Mania <amj2544@tutanota.com>
2024-05-31 23:27:04 +0200

    bootstrap4

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,8 +6,7 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
-
+# Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
@@ -15,5 +14,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 # Rails.application.config.assets.precompile += %w(application.js)
-# verifier si utile!!!
+# # verifier si utile!!!
 # Rails.application.config.session_store :active_record_store
