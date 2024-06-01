67ce1d7d0080a0028b503b4b7969d6d7b5206e01
Anthony Mania <amj2544@tutanota.com>
2024-06-01 22:09:34 +0200

    ppush in master?

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,6 +1,10 @@
 require 'cloudinary'
 
 Cloudinary.config_from_url("cloudinary://947717511563492:BYJ-euBcX9Ik2EorjeOUASyUHBg@hnnyhemvj")
+
 Cloudinary.config do |config|
+  config.cloud_name = 'hnnyhemvj'
+  config.api_key = '947717511563492'
+  config.api_secret = 'BYJ-euBcX9Ik2EorjeOUASyUHBg'
   config.secure = true
 end
