404e70c3e465f19741f3631768ab596bb5fd491b
Anthony Mania <amj2544@tutanota.com>
2024-06-01 16:14:02 +0200

    config.rb

---
+++ config/config.rb
@@ -0,0 +1,11 @@
+    development:
+      cloud_name: "hnnyhemvj"
+      api_key: "947717511563492"
+      api_secret: "***************************"
+      secure: true
+
+    production:
+      cloud_name: "hnnyhemvj"
+      api_key: "947717511563492"
+      api_secret: "***************************"
+      secure: true
