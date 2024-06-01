4a2b7d134047a96c1b9cbc3856d048e6133cb3a8
Anthony Mania <amj2544@tutanota.com>
2024-06-01 16:26:21 +0200

    configrb + quistart.rb

--- config/config.rb
+++ config/config.rb
@@ -1,3 +1,8 @@
+require 'cloudinary'
+
+Cloudinary.config_from_url("cloudinary://API_KEY:API_SECRET@CLOUD_NAME")
+
+
     development:
       cloud_name: "hnnyhemvj"
       api_key: "947717511563492"
