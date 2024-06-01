caee410bcd5c695a94b49a64e8927258cff474de
Anthony Mania <amj2544@tutanota.com>
2024-06-01 18:04:51 +0200

    cloudninary nouveau

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,6 +1,9 @@
 require 'cloudinary'
 
 Cloudinary.config_from_url("cloudinary://947717511563492:BYJ-euBcX9Ik2EorjeOUASyUHBg@hnnyhemvj")
+Cloudinary.config do |config|
+  config.secure = true
+end
 
 # development:
 #   cloud_name: "hnnyhemvj"
