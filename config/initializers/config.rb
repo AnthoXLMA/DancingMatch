2f52850dda5cba2dfac6f934ebab0a869ee8ffa5
Anthony Mania <amj2544@tutanota.com>
2024-06-01 16:41:57 +0200

    develoment config

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,16 +1,3 @@
 require 'cloudinary'
 
 Cloudinary.config_from_url("cloudinary://947717511563492:BYJ-euBcX9Ik2EorjeOUASyUHBg@hnnyhemvj")
-
-
-    development:
-      cloud_name: "hnnyhemvj"
-      api_key: "947717511563492"
-      api_secret: "***************************"
-      secure: true
-
-    production:
-      cloud_name: "hnnyhemvj"
-      api_key: "947717511563492"
-      api_secret: "***************************"
-      secure: true
