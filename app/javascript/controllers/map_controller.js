4db644ecf81af6b0b23aff05389da14cc58f4df0
Anthony Mania <amj2544@tutanota.com>
2024-06-29 17:21:02 +0200

    apikey

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -17,16 +17,27 @@ export default class extends Controller {
   }
 }
 
+connect() {
+  this.#addMarkersToMap()
+}
 
-// connect() {
-//   // [...]
-//   this.addMarkersToMap()
-// }
+#addMarkersToMap() {
+  this.markersValue.forEach((marker) => {
+    new mapboxgl.Marker()
+      .setLngLat([ marker.lng, marker.lat ])
+      .addTo(this.map)
+  })
+}
 
-// addMarkersToMap() {
-//   this.markersValue.forEach((marker) => {
-//     new mapboxgl.Marker()
-//       .setLngLat([ marker.lng, marker.lat ])
-//       .addTo(this.map)
-//   })
-// }
+connect() {
+  // [...]
+  this.#addMarkersToMap()
+  this.#fitMapToMarkers()
+}
+
+// [...]
+#fitMapToMarkers() {
+  const bounds = new mapboxgl.LngLatBounds()
+  this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+  this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+}
