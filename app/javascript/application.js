ae248c8ea450e47a0f704924092632c28d049445
Anthony Mania <amj2544@tutanota.com>
2024-06-30 10:01:35 +0200

    Node Updated

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,10 +3,12 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+
+// require("@rails/ujs").start()
+// require("turbolinks").start()
+// require("@rails/activestorage").start()
+// require("channels")
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -18,23 +20,23 @@ require("channels")
 // WRITE YOUR OWN JS STARTING FROM HERE 👇
 // ----------------------------------------------------
 
-import { Application } from "@hotwired/stimulus"
-window.Stimulus = Application.start()
+// import { Application } from "@hotwired/stimulus"
+// window.Stimulus = Application.start()
 
 // CSS
-import 'mapbox-gl/dist/mapbox-gl.css';
-import "bootstrap";
-import "./src/application.scss";
-import "@popperjs/core";
+// import 'mapbox-gl/dist/mapbox-gl.css';
+// import "bootstrap";
+// import "./src/application.scss";
+// import "@popperjs/core";
 
 // internal imports
-import { initMapbox } from '../plugins/init_mapbox';
-import { initClickMe } from '../plugins/init_click_me';
-import { selectEvent } from '../plugins/select_event';
-import { addElement } from '../plugins/text';
+// import { initMapbox } from '../plugins/init_mapbox';
+// import { initClickMe } from '../plugins/init_click_me';
+// import { selectEvent } from '../plugins/select_event';
+// import { addElement } from '../plugins/text';
 // import { initChart } from '../plugins/init_chart';
-import { initSiema } from '../plugins/init_siema';
-import { initGraphix } from '../plugins/init_compare';
+// import { initSiema } from '../plugins/init_siema';
+// import { initGraphix } from '../plugins/init_compare';
 // import { initFlatpickr } from '../plugins/flatpickr';
 // import flatpickr from "flatpickr"
 // import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
@@ -42,15 +44,15 @@ import { initGraphix } from '../plugins/init_compare';
 // import { initFlatpickr } from '../plugins/initFlatpickr';
 // SUPPRIME LA MAP QUAND EST ACTIVE : FLATPICKR //
 
-document.addEventListener('turbolinks:load', () => {
-  initMapbox();
-  initGraphix();
+// document.addEventListener('turbolinks:load', () => {
+//   initMapbox();
+//   initGraphix();
   // initClickMe();
   // initFlatpickr();
   // ChangeSlide();
   // initCalendar();
   // selectEvent();
-});
+// });
 
 // External imports
 // import swal from 'sweetalert';
@@ -61,4 +63,4 @@ document.addEventListener('turbolinks:load', () => {
 // import "fichier js "
 // import initNavTabs from '../components/init_nav_tabs';
 
-import "controllers"
+// import "controllers"
