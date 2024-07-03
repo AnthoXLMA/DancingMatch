7c9cb2b5c925f1a76a42d24a31deed717273aa11
Anthony Mania <amj2544@tutanota.com>
2024-07-03 19:01:05 +0200

    NpmupdatingForMapBox

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,7 +7,7 @@ pin "@popperjs/core", to: "popper.js", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin "mapbox-gl", to: "https://ga.jspm.io/npm:mapbox-gl@3.1.2/dist/mapbox-gl.js"
+pin "mapbox-gl", to: "https://ga.jspm.io/npm:mapbox-gl@3.4.0/dist/mapbox-gl.js"
 pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/process-production.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
