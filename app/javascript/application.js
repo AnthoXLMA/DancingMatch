cee093460b31b57a5b1a2428a2f74f3984b6f3d7
Anthony Mania <amj2544@tutanota.com>
2024-07-04 18:13:44 +0200

    commented JS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,66 +1,66 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+// // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// // This file is automatically compiled by Webpack, along with any other files
+// // present in this directory. You're encouraged to place your actual application logic in
+// // a relevant structure within app/javascript and only use these pack files to reference
+// // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+// require("@rails/ujs").start()
+// require("turbolinks").start()
+// require("@rails/activestorage").start()
+// require("channels")
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
+// // Uncomment to copy all static images under ../images to the output folder and reference
+// // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// // or the `imagePath` JavaScript helper below.
 
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-// ----------------------------------------------------
-// Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
-// WRITE YOUR OWN JS STARTING FROM HERE 👇
-// ----------------------------------------------------
+// // const images = require.context('../images', true)
+// // const imagePath = (name) => images(name, true)
+// // ----------------------------------------------------
+// // Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
+// // WRITE YOUR OWN JS STARTING FROM HERE 👇
+// // ----------------------------------------------------
 
-import { Application } from "@hotwired/stimulus"
-window.Stimulus = Application.start()
+// import { Application } from "@hotwired/stimulus"
+// window.Stimulus = Application.start()
 
-// CSS
-import 'mapbox-gl/dist/mapbox-gl.css';
-import "bootstrap";
-import "./src/application.scss";
-import "@popperjs/core";
+// // CSS
+// import 'mapbox-gl/dist/mapbox-gl.css';
+// import "bootstrap";
+// import "./src/application.scss";
+// import "@popperjs/core";
 
-// internal imports
-// import { initMapbox } from '../plugins/init_mapbox';
-// import { initClickMe } from '../plugins/init_click_me';
-// import { selectEvent } from '../plugins/select_event';
-// import { addElement } from '../plugins/text';
-// import { initChart } from '../plugins/init_chart';
-// import { initSiema } from '../plugins/init_siema';
-// import { initGraphix } from '../plugins/init_compare';
+// // internal imports
+// // import { initMapbox } from '../plugins/init_mapbox';
+// // import { initClickMe } from '../plugins/init_click_me';
+// // import { selectEvent } from '../plugins/select_event';
+// // import { addElement } from '../plugins/text';
+// // import { initChart } from '../plugins/init_chart';
+// // import { initSiema } from '../plugins/init_siema';
+// // import { initGraphix } from '../plugins/init_compare';
 
-// import { initFlatpickr } from '../plugins/flatpickr';
+// // import { initFlatpickr } from '../plugins/flatpickr';
 
-// import flatpickr from "flatpickr"
-// import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
+// // import flatpickr from "flatpickr"
+// // import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
+// // // SUPPRIME LA MAP QUAND EST ACTIVE : FLATPICKR //
+// // import { initFlatpickr } from '../plugins/initFlatpickr';
 // // SUPPRIME LA MAP QUAND EST ACTIVE : FLATPICKR //
-// import { initFlatpickr } from '../plugins/initFlatpickr';
-// SUPPRIME LA MAP QUAND EST ACTIVE : FLATPICKR //
 
-document.addEventListener('turbolinks:load', () => {
-  // initMapbox();
-  // initGraphix();
-  // initClickMe();
-  // initFlatpickr();
-  // ChangeSlide();
-  // initCalendar();
-  // selectEvent();
-});
+// document.addEventListener('turbolinks:load', () => {
+//   // initMapbox();
+//   // initGraphix();
+//   // initClickMe();
+//   // initFlatpickr();
+//   // ChangeSlide();
+//   // initCalendar();
+//   // selectEvent();
+// });
 
-// External imports
-import swal from 'sweetalert';
+// // External imports
+// import swal from 'sweetalert';
 
-// Internal imports, e.g:
-import { initSelect2 } from '../components/init_select2';
-import "fichier js "
-import initNavTabs from '../components/init_nav_tabs';
-import "controllers"
+// // Internal imports, e.g:
+// import { initSelect2 } from '../components/init_select2';
+// import "fichier js "
+// import initNavTabs from '../components/init_nav_tabs';
+// import "controllers"
