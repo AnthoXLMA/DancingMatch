fe9ebcdfb5fa7f38eac8bd28b3342729113dfb02
Anthony Mania <amj2544@tutanota.com>
2024-07-05 18:28:31 +0200

    new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,10 +19,10 @@ class UsersController < ApplicationController
       {
         lat: user.latitude,
         lng: user.longitude,
-        # infoWindow: render_to_string(partial: "info_window", locals: { user: user }),
-        # image_url: helpers.asset_url('mapbox-marker-icon-green.svg'),
-        info_window_html: render_to_string(partial: "info_window", locals: {user: user}),
-        marker_html: render_to_string(partial: "marker")
+        infoWindow: render_to_string(partial: "info_window", locals: { user: user }),
+        image_url: helpers.asset_url('mapbox-marker-icon-green.svg'),
+        # info_window_html: render_to_string(partial: "info_window", locals: {user: user}),
+        # marker_html: render_to_string(partial: "marker")
       }
     end
     @mapmarkers = @markers.to_json
